282d4530b261b5bba900817bcdf1b79374c2a124
Nathan Jones <nathan.jones@ironmail.org>
2016-07-05 07:49:49 -0600

    use es6 module syntax to import jquery

--- src/scripts/graduation-progress/js/gradprogress.js
+++ src/scripts/graduation-progress/js/gradprogress.js
@@ -1,7 +1,7 @@
-/*global $j*/
-(function() {
-    'use strict';
-    var creditsTemplate = $j($j('#credits-template').html());
-    var creditsSelect = $j('table').eq(1);
-    creditsTemplate.insertBefore(creditsSelect);
+import $ from 'jquery';
+
+(function(){
+  const creditsTemplate = $('#credits-template').html();
+  const creditsSelect = $('table').eq(1);
+  creditsSelect.before(creditsTemplate);
 }());
