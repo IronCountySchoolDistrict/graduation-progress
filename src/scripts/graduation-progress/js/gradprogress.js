5934839f8edbe8f4a7c88e2f0590328bbb84fd2c
basiczombie <issacgable@gmail.com>
2017-01-05 11:22:03 -0700

    reformated to ES2015

--- src/scripts/graduation-progress/js/gradprogress.js
+++ src/scripts/graduation-progress/js/gradprogress.js
@@ -1,6 +1,7 @@
+import $ from "jquery";
 
-(function($){
-  const creditsTemplate = $('#credits-template').html();
-  const creditsSelect = $('table').eq(1);
-  creditsSelect.before(creditsTemplate);
-}($j));
+export default function() {
+    const creditsTemplate = $('#credits-template').html();
+    const creditsSelect = $('table').eq(1);
+    creditsSelect.before(creditsTemplate);
+}
